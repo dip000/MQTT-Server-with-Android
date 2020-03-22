3a40c038b0f215a5ee13d49e5f01d9bd48fed695
dip000 <58742147+dip000@users.noreply.github.com>
2020-03-22 13:10:58 -0600

    Web Client for MQTT Server with Android v1.3

--- WebClient/WebClient.js
+++ WebClient/WebClient.js
@@ -1,10 +1,10 @@
-	/*projects here*/
 			var target=null;
 	
 			const project_LED = '<i id="actionImg" class="material-icons" onclick="toggleLED()">toggle_off</i>';
 			var LEDon = false;
 
-			const project_Messager = '<div id="webConsoleMessageBox"> </div> <div id="webConsoleMessageInput"><input type="text"></input><i onclick="MessagerSend();" class="material-icons">play_circle_outline</i></div>';
+			const project_Messager = '<div id="webConsoleMessageBox"> </div> <div id="webConsoleMessageInput"><input type="text"></input><i onclick="MessagerSend();" class="material-icons">send</i></div><div id="webConsoleMessageButtons"><i class="material-icons"><span onclick="messageButtons(0);">person_add</span> <span onclick="messageButtons(1);">person_outline</span><span onclick="messageButtons(2);">cloud_upload</span><span onclick="messageButtons(3);">cloud_download</span><span onclick="messageButtons(4);">call</span><span onclick="messageButtons(5);">call_end</span><span onclick="messageButtons(6);">mail</span></i></div>';
+			const messageFromCodes = {"404":"Target not found", "304":"Command not found", "204":"User name not found","200":"User banned","210":"You need Administrator privileges","420":"Connect first with $con:~ ", "590":"send/get not found","690":"Incorrect syntax for save command","0":"Success!"}; 
 			var isMessager = false;
 			
 			
@@ -26,7 +26,7 @@
 			function showMessage(msg, incoming=true){
 				let from = activeTarget===null? subscribeTopic : activeTarget;
 				let receivedMessage = '<div><span>'+ from +'</span>'+ msg +'</div>';
-				let	sentMessage = '<div style="background:rgba(0,180,20,0.14); margin-left:15%;"><span>'+ usrKey +'</span>'+ msg +'</div>';
+				let	sentMessage = '<div style="background:#c0ffc0; margin-left:15%;"><span>'+ usrKey +'</span>'+ msg +'</div>';
 
 				$("#webConsoleMessageBox").append(incoming===true?receivedMessage:sentMessage);
 				$("#webConsoleMessageBox").scrollTop($("#webConsoleMessageBox")[0].scrollHeight);
@@ -38,10 +38,23 @@
 				MQTTsend(input + " -" + usrKey,publishTopic);
 				showMessage(input, false);
 			}
+			
+			function messageButtons(n){
+				let message;
+				
+				switch(n){
+					case 0: message = "$add:~"; break;
+					case 1: message = "$del:~"; break;
+					case 2: message = "$save:~"; break;
+					case 3: message = "$get:~"; break;
+					case 4: message = "$con:~"; break;
+					case 5: message = "$shut:~"; break;
+					case 6: message = "$send:~"; break;
+				}
+				$("#webConsoleMessageInput input").val(message);
+			}
 
-
-	/*Client Build Here*/
-  
+	/*This script is the one uploaded to gitHub "MQTTSSL.js". Update when finished*/
 			var usrKey;
 			var warnings=true;
 			var connectionState=false;
@@ -107,8 +120,6 @@
 			function toggleWarnings(){
 				warnings = !warnings;
 			}
-      
- /*Paho MQTT Here*/
 
 			function MQTTmake() {
 				try{
@@ -167,9 +178,17 @@
 			}
 			
 			function onMessageArrived(message) {
-				receivedMessage = message.payloadString;
-				
-				if(isMessager == false){
+				let receivedMessage = message.payloadString;
+					
+				if(isMessager === true){
+					if(receivedMessage in messageFromCodes){
+						showMessage(messageFromCodes[receivedMessage]);
+					}
+					else{
+						showMessage(receivedMessage);
+					}
+				}
+				else if(isMessager === false){
 					clearTimeout(responseTimeout);
 					if(receivedMessage === "0"){
 						console.log("Logged succesfuly!");
@@ -177,28 +196,21 @@
 						$("#webConsoleControl").show();
 						isMessager = true;
 					}
-					else if(receivedMessage === "204"){
-						client.disconnect();
-						sendConsoleMessage("Wrong key!");
-					}
-					else if(receivedMessage === "200"){
-						client.disconnect();
-						sendConsoleMessage("This user is banned");
-					}
-					else{
-						client.disconnect();
-						sendConsoleMessage("Server refused. Reason: " + receivedMessage);
-					}
-					console.log("onMessageArrived:"+receivedMessage);
 					receivedMessage = "";
 					readyToResend = true;
 				}
-				else{
-					showMessage(receivedMessage);
-				}
 			}
 			
-			
+			function toText(input){
+				for(code in messageFromCodes){
+					if(input === code){
+						return messageFromCodes[code];
+					}
+					else{
+						return false;
+					}
+				}
+			}
 			
 			function sendConsoleMessage(message){
 				if(warnings == true){
